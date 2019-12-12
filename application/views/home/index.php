38385e7f3bacbdcf7aad9cd1c41e52c0c4061f15
Bilkis Ismail <bilkisismail07@gmail.com>
2019-12-12 16:37:41 +0700

    Add animate css

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,7 +1,7 @@
 </head>
 
 <body>
-    <div class="container mt-5">
+    <div class="container mt-5 animated slideInLeft">
         <div class="jumbotron">
             <h1 class="display-4">Hello, world!</h1>
             <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -6,6 +6,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="theme-color" content="#27B532">
+    <meta name="description" content="My Setup for using Codeigniter 3 & Bootsrap 4.4">
+    <meta name="author" content="kubicode">
+
     <meta data-rh="true" property="og:title" content="Build for fun" />
     <meta data-rh="true" property="og:site_name" content="kubicode" />
     <meta data-rh="true" property="og:description" content="My Setup for using Codeigniter 3 & Bootsrap 4.4" />
@@ -14,6 +17,10 @@
     <!-- Bootstrap CSS -->
     <link rel="shortcut icon" href="<?= asset_url() . 'img/favicon.png' ?>" type="image/x-icon">
     <link rel="stylesheet" href="<?= asset_url() . 'css/bootstrap.min.css' ?>">
+    <link rel="stylesheet" href="<?= asset_url() . 'css/animate.min.css' ?>">
+
+    <!-- Jquery JS -->
+    <script src="<?= asset_url() . 'js/jquery-3.4.1.min.js' ?>"></script>
 
     <!-- Get title from controller -->
     <title><?= $title ?></title>
